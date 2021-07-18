d98e50232a98a288f1bca5bd7f4586a936818a26
vanquan410 <vanquan4101999@gmail.com>
2021-07-18 23:08:24 +0700

    feat: Tạo dưn án mới

---
+++ project1/src/components/icons/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+DVQuanICon.propTypes = {
+
+};
+DVQuanICon.defaultProps = {
+
+};
+function DVQuanICon(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default DVQuanICon;
